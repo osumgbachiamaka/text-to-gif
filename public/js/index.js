c5a8505705fe1d5207bcc7f331c2a8b9f13da5f5
Osumgba Chiamaka <osumgbachiamaka@gmail.com>
2019-11-16 18:03:54 +0100

    The search api implemented

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
-const gif = "http://api.giphy.com/v1/gifs/translate?";
+const gif = "http://api.giphy.com/v1/gifs/";
+const translate = 'translate';
+const search = 'search';
 const api_key = "QeuRpqUf9nYgvIzxiS2xqDRVtQJUZc7t";
 const retrievedData = {}
 
@@ -10,11 +12,12 @@ btn.addEventListener('click', (e) => {
 const fetchApi = () => {
     const text = document.querySelector('input').value
     fetchTransalateApi(text)
+    fetchSearchApi(text)
 }
 
 const fetchTransalateApi = (text) => {
     const query = `s=${text}&`;
-    const api = gif + query + `api_key=${api_key}`;
+    const api = `${gif}${translate}?${query}api_key=${api_key}`;
 
     console.log('about to fetch')
     fetch(api)
@@ -23,15 +26,31 @@ const fetchTransalateApi = (text) => {
     .catch(err => console.log(err))
 }
 
+const fetchSearchApi = (text) => {
+    const query = `q=${text}&`;
+    const api = `${gif}${search}?${query}api_key=${api_key}&limit=6`;
+
+    fetch(api)
+    .then(response => response.json())
+    .then((data) => destructureSearchApi(data))
+    .catch(err => console.log(err))
+}
+
 const destructureTranslateApi = (result) =>{
     const {data} = result
     // console.log(data.images.original)
     retrievedData.data = data.images.original.url;
     retrievedData.user = data.user;
-    showApi(retrievedData);
+    showTranslateApi(retrievedData);
 }
 
-const showApi = (results) => {
+const destructureSearchApi = (result) =>{
+    const {data} = result
+    // console.log(data)
+    showSearchApi(data);
+}
+
+const showTranslateApi = (results) => {
     //destructuring result
     const {data, user} = results
     const gifDisplay = document.querySelector('.gif')
@@ -46,6 +65,28 @@ const showApi = (results) => {
         <img src = '${data}' class='translateGif' alt = >
     `
     gifDisplay.insertAdjacentHTML('beforeend', image)
+}
+
+const showSearchApi = (result) => {
+    const table = document.querySelector('.gif--search .search--row')
+    
+    result.forEach(element => {
+        const data = element.images.original.url;
+        
+        let tableRow = `
+            <td class="search--column">
+                <div class='copyDiv'>
+                    <input type = 'text' value = '${data}' readonly class='url'>
+                    <button onclick="myFunction()" onmouseout="outFunc()">
+                    <span class="tooltiptext">Copy to clipboard</span>
+                        Copy url
+                    </button>
+                </div>
+                <img src = '${data}' class='translateGif' alt = >
+            </td>
+        `
+        table.insertAdjacentHTML("beforeend", tableRow)
+    });
 }
 
 const myFunction = () => {
